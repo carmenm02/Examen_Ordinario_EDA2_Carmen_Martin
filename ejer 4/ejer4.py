d8784f5bf7fc6081e75ccabb15550e24c6793938
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-20 00:09:17 +0100

    ejer 4

--- ejer 4/ejer4.py
+++ ejer 4/ejer4.py
@@ -13,4 +13,12 @@ class ArtefactosValiosos:
     def sortByDate(self, elem):
         return datetime.strptime(elem.fechaCaducidad, '%d/%m/%Y')
     
-    
+    def usarFuerza(mochila,numObjetos):
+        if (len(mochila)>0):
+            if mochila[len(mochila) - 1].nombre == "Sable de luz":
+                print("Se han sacado" + str(numObjetos) + "objetos de la mochila")
+            else:
+                print("No hay mas objetos en la mochila")
+numObjetos = 0
+mochila = [ArtefactosValiosos("Linterna", 500, 20, "05/28/2022"), ArtefactosValiosos("Sable de Luz", 500, 20, "05/28/2022"), ArtefactosValiosos("Esmeralda", 500, 20, "05/28/2022") ,ArtefactosValiosos("Diamante", 500, 20, "05/28/2022")]
+usarFuerza(mochila,numObjetos)
