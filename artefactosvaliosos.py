40af2ba2990ff1902e4ca07f0b4c60e6752dcf70
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 23:21:45 +0100

    3

--- artefactosvaliosos.py
+++ artefactosvaliosos.py
@@ -7,4 +7,11 @@ class ArtefactosValiosos:
         self.precio = precio
         self.fecha = fecha
         print("Artefacto creado con éxito")
-        
+    
+    def __str__(self):
+        return '\nNombre: ' + self.nombre + '\nPeso: ' + self.peso + '\nPrecio: ' + self.precio + '\nFecha: ' + self.fecha
+    
+    def sortByDate(self, elem):
+        return datetime.strptime(elem.fechaCaducidad, '%d/%m/%Y')
+    
+    
