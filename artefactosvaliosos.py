8601ad75c5f2aeb9e2fe0abc8a4502ec1dc33f5c
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 23:20:25 +0100

    ejer 3

---
+++ artefactosvaliosos.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class ArtefactosValiosos:
+    def __init__(self, nombre, peso, precio, fecha):
+        self.nombre = nombre
+        self.peso = peso
+        self.precio = precio
+        self.fecha = fecha
+        print("Artefacto creado con éxito")
+        
