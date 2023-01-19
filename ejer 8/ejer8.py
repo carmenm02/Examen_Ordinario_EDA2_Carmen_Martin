6c30f7120ad192ab82f6cdc2e42aa8ee502b1ba0
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-20 00:12:44 +0100

    8

---
+++ ejer 8/ejer8.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import random
+
+g = nx.Graph()
+planetas = ['Alderaan', 'Endor', 'Dagobah', 'Hoth', 'Tatooine', 'Kamino', 'Naboo', 'Mustafar', 'Scarif', 'Bespin', 'Cantonica', 'DQar', 'Dantooine', 'Atollon', 'Jedha', 'Corellia', 'Coruscant']
+
+g.add_node('Alderaan')
+g.add_node('Endor')
+g.add_node('Dagobah')
+g.add_node('Hoth')
+g.add_node('Tatooine')
+g.add_node('Kamino')
+g.add_node('Naboo')
+g.add_node('Mustafar')
+g.add_node('Scarif')
+g.add_node('Bespin')
+g.add_node('Cantonica')
+g.add_node('DQar')
+g.add_node('Dantooine')
+g.add_node('Atollon')
+g.add_node('Jedha')
+g.add_node('Corellia')
+g.add_node('Coruscant')
+
