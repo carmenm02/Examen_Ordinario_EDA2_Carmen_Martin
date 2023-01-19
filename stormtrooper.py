7537a760095cabfeced99b61ea2d5ad490b5e919
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 20:06:27 +0100

    ejer 1

--- stormtrooper.py
+++ stormtrooper.py
@@ -1,4 +1,12 @@
-class Stormtrooper:
+class Stormtrooper:      
     def __init__(self,name,rango) :
         self.name = name
         self.rango = rango
+        print("Stormtrooper creado con éxito")
+
+    def calificacion(self):
+        rangos = {"cadete": 1, "cabo": 2, "sargento": 3, "teniente": 4, "capitan": 5}
+        if self.rango in rangos:
+            return rangos[self.rango]
+        else:
+            return "Rango desconocido"
