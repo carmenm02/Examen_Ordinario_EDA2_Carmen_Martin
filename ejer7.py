8ae62db33fb99d23476855df52011b25c292bbc7
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 23:07:23 +0100

    .

--- ejer7.py
+++ ejer7.py
@@ -10,3 +10,12 @@ class NodoArbol:
         lista_nodos = sorted(lista_nodos,key=lambda x: x.simbolo)
         lista_nodos = sorted(lista_nodos,key=lambda x: x.freq)
         return lista_nodos
+    
+    def insertar_nodo(lista_nodos,nodo):
+        for i in range(len(lista_nodos)):
+            if nodo.freq < lista_nodos[i].freq:
+                lista_nodos.insert(i,nodo)
+            if i == len(lista_nodos)-1:
+                lista_nodos.append(nodo)
+                break
+        return lista_nodos
