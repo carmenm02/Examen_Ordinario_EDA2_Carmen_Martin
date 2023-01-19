1e4b3a94198687396113a202550c83a69e20f5f0
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 23:27:03 +0100

    main 1

---
+++ ejer 1/main.py
@@ -0,0 +1,11 @@
+from stormtrooper import *
+if __name__ == '__main__':
+    stormtrooper = Stormtrooper('AK-3654', 7)
+    print(stormtrooper.nombre)
+    print(stormtrooper.rango)
+    stormtrooper.clasificacion()
+    print(stormtrooper.nombre.split("-")[0])
+    print(stormtrooper.nombre.split("-")[1][0])
+    print(stormtrooper.nombre.split("-")[1][1])
+    print(stormtrooper.nombre.split("-")[1][2])
+    print(stormtrooper.nombre.split("-")[1][3])
