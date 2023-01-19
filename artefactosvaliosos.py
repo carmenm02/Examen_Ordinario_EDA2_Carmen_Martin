8d60b966719a741d135a7fdaee2b2d7960815f0d
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-19 23:24:18 +0100

    ejer 3

--- artefactosvaliosos.py
+++ artefactosvaliosos.py
@@ -14,4 +14,10 @@ class ArtefactosValiosos:
     def sortByDate(self, elem):
         return datetime.strptime(elem.fechaCaducidad, '%d/%m/%Y')
     
-    
+lista = [ArtefactosValiosos("Artefacto 1", "10kg", "1000$", "01/01/2020"), ArtefactosValiosos("Artefacto 2", "20kg", "2000$", "02/02/2020"), ArtefactosValiosos("Artefacto 3", "30kg", "3000$", "03/03/2020"), ArtefactosValiosos("Artefacto 4", "40kg", "4000$", "04/04/2020")]
+lista.sort(key=sortByDate)
+for stormtrooper in lista:
+    print(stormtrooper)
+print("Precio cambiado")
+lista[0].precio = 4000
+print(lista[0])
