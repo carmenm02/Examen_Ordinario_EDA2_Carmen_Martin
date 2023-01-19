3b5197f8de23a944fb063ba3f8642bf2f80605f0
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2023-01-20 00:31:44 +0100

    6

--- ejer6/ejer6.py
+++ ejer6/ejer6.py
@@ -13,4 +13,17 @@ class Stormtrooper:
         print("Identificador de siglo: " + self.name.split("-")[1][1])
         print("Identificador de escuadra: " + self.name.split("-")[1][2])
         print("Numero de trooper: " + self.name.split("-")[1][3])
+    def __str__(self):
+        return '\nNombre: ' + self.name + '\nRango: ' + str(self.rango)
     
+    lista = ["FL","TF","TK","CT","FN","FO"]
+    for i in range(2000):
+        numRandom = random.randint(0,5)
+        listanum = []
+        for j in range(4):
+            listanum.append(random.randint(0,9))
+
+        num = "".join(listanum)
+        codlegion = lista[numRandom]
+        Stormtrooper = Stormtrooper(codlegion + "-" + num, 7)
+        print(Stormtrooper)
